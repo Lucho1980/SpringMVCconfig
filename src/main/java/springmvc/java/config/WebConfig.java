9facbae27d21e088bdac9e6cff89f4eb69a89b80
Luciano-Pc <Luciano-Pc@DESKTOP-LUCIANO>
2017-07-16 15:16:34 -0300

    first commit

---
+++ src/main/java/springmvc/java/config/WebConfig.java
@@ -0,0 +1,55 @@
+package springmvc.java.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+//Configuration class dispatcher-serverlet.xml
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages={"springmvc.java"})
+@Import((ApplicationContext.class))
+@PropertySource("classpath:application.properties")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	//index mapping
+	registry.addViewController("/").setViewName("index");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+		
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		//strings to view
+		//success from a controller -> prefix/successs/suffix
+		//success from a controller -> /WEB-INF/pages/success.jsp
+		
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+		
+	}
+
+}
---
+++ src/main/java/springmvc/java/service/impl/BlogPostServiceImpl.java
@@ -0,0 +1,25 @@
+package springmvc.java.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import springmvc.java.domain.BlogPost;
+import springmvc.java.domain.DataSource;
+import springmvc.java.service.BlogPostService;
+
+public class BlogPostServiceImpl implements BlogPostService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(BlogPostServiceImpl.class);
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	public void savePost(BlogPost blogPost) {
+		LOGGER.debug("BlogPostServiceImpl: "+dataSource.getUrl());
+		LOGGER.debug("BlogPostServiceImpl: savePost is called ");
+		
+	}
+
+}
